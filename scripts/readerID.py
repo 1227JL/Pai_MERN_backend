80dc67ae90f173df30df2db2e05a5d9804a3fc0d
1227JL <lopezvidaljuliandavid@gmail.com>
2024-04-10 13:11:19 -0500

    Finalización de estructuración básica del script readerID.py

--- scripts/readerID.py
+++ scripts/readerID.py
@@ -49,28 +49,24 @@ def extract_text_from_pdf(pdf_path):
 
 
 def formated_information(text):
-    # Expresiones regulares para buscar la información
-    name_regex = r"Nombres\n([\w\s]+)"
-    surname_regex = r"NUIP [\d\.]+\n([\w\s]+)"
+    name_pattern = r"\n([A-Z]+)<([A-Z]+)<<([A-Z]+)<([A-Z]+)<<"
+    matches = re.search(name_pattern, text)
+    if matches:
+        surname1, surname2, name1, name2 = matches.groups()
+        full_name = f"{name1} {name2} {surname1} {surname2}".replace('<', ' ').strip()
+    else:
+        full_name = None
+        
     id_regex = r"NUIP ([\d\.]+)"
-    birth_date_regex = r"Fecha de nacimiento. GS a\n\n([\d\w\s]+)"
+    birth_date_regex = r"Fecha de nacimiento.*\n\n([\d\w\s]+)"
     blood_type_regex = r"([\w\+\-]+)\n>"
 
-    # Buscar en el texto
-    name_match = re.search(name_regex, text)
-    surname_match = re.search(surname_regex, text)
     id_match = re.search(id_regex, text)
     birth_date_match = re.search(birth_date_regex, text)
     blood_type_match = re.search(blood_type_regex, text)
 
-    # Extraer y limpiar la información
-    name = name_match.group(1).strip() if name_match else None
-    surname = surname_match.group(1).strip() if surname_match else None
-    full_name = f"{name} {surname}" if name and surname else None
-    # Eliminamos los puntos del ID
     id_number = id_match.group(1).replace('.', '') if id_match else None
-
-    # Convertir la fecha de nacimiento a un formato legible
+    birth_date = None
     months = {
         "ENE": "01", "FEB": "02", "MAR": "03", "ABR": "04", "MAY": "05", "JUN": "06",
         "JUL": "07", "AGO": "08", "SEP": "09", "OCT": "10", "NOV": "11", "DIC": "12"
@@ -79,25 +75,20 @@ def formated_information(text):
         day, month_abbr, year, _ = birth_date_match.group(1).split()
         month = months.get(month_abbr.upper(), "00")
         birth_date = f"{day}/{month}/{year}"
-
     blood_type = blood_type_match.group(1).strip() if blood_type_match else None
 
-    # Crear un objeto JSON con la información
     person_info = {
         "Nombre": full_name,
         "ID": id_number,
         "Nacimiento": birth_date,
         "RH": blood_type
     }
-
-    # Convertir el objeto Python a una cadena JSON
     json_info = json.dumps(person_info, ensure_ascii=False)
-
-    # Ahora `json_info` contiene la información en formato JSON
     print(json_info)
 
 # Replace 'your_pdf_file.pdf' with your actual PDF file path
 pdf_path = 'test/Cedula_de_ciudadanía.pdf'
 extracted_text = extract_text_from_pdf(pdf_path)
+print(extracted_text)
 user_information = formated_information(extracted_text)
 
