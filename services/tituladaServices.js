6bfb5841ec90f0c55715265d9fcd95f4ee9640c7
1227JL <lopezvidaljuliandavid@gmail.com>
2024-05-19 16:55:05 -0500

    Implementación de funciones de servicios para la titulada

--- services/tituladaServices.js
+++ services/tituladaServices.js
@@ -14,49 +14,51 @@ const getFileTitulada = async (req, res) => {
   }
 };
 
+
 const obtenerAprendices = async (req, res) => {
-  const { id } = req.params;
-  const { aprendices } = await Titulada.findById(id).populate({
-    path: "aprendices",
-    select: "nombre telefono tipoDocumento rh estado email documento",
-  });
-  res.json(aprendices);
-};
-
-const obtenerInstructores = async (req, res) => {
-  try {
     const { id } = req.params;
-
-    // Encontrar todos los instructores cuyo array de tituladas incluya el id especificado
-    const titulada = await Titulada.findById(id).populate({
-      path: "instructores.instructor",
-      select: "nombre identificacion",
+    const { aprendices } = await Titulada.findById(id).populate({
+      path: "aprendices",
+      select: "nombre telefono tipoDocumento rh estado email documento",
     });
-
-    if (!titulada) {
-      return res.status(404).json({ message: "Titulada no encontrada" });
+    res.json(aprendices);
+  };
+  
+  const obtenerInstructores = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      // Encontrar todos los instructores cuyo array de tituladas incluya el id especificado
+      const titulada = await Titulada.findById(id).populate({
+        path: "instructores.instructor",
+        select: "nombre identificacion",
+      });
+  
+      if (!titulada) {
+        return res.status(404).json({ message: "Titulada no encontrada" });
+      }
+  
+      // Extraer los instructores y el estado de aCargo
+      const instructores = titulada.instructores.map((instructorObj) => ({
+        _id: instructorObj._id,
+        nombre: instructorObj.instructor.nombre,
+        identificacion: instructorObj.instructor.identificacion,
+        aCargo: instructorObj.aCargo,
+      }));
+  
+      res.json(instructores);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error al obtener los instructores" });
     }
-
-    // Extraer los instructores y el estado de aCargo
-    const instructores = titulada.instructores.map((instructorObj) => ({
-      _id: instructorObj._id,
-      nombre: instructorObj.instructor.nombre,
-      identificacion: instructorObj.instructor.identificacion,
-      aCargo: instructorObj.aCargo,
-    }));
-
-    res.json(instructores);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error al obtener los instructores" });
+  };
+  
+  const obtenerCompetencias = async (req, res) => {
+    const { id } = req.params
+  
+    const { competencias } = await Titulada.findById(id).select('competencias')
+    res.json(competencias)
   }
-};
 
-const obtenerCompetencias = async (req, res) => {
-  const { id } = req.params
-
-  const { competencias } = await Titulada.findById(id).select('competencias')
-  res.json(competencias)
-}
 
 export { getFileTitulada, obtenerAprendices, obtenerInstructores, obtenerCompetencias };
